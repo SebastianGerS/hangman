5ecd31c1fe151438b58421fd095e4599dde838a5
Sebastian Gerstel Sollerman <sebastian.gerstel.sollerman@chasacademy.se>
2017-10-11 17:12:55 +0200

    Javascript/HTML/CSS JavaScript: *fix letterChercker() now transforms both user guess and selectedword (or more corectly a copy of) to uppercase to make it possible to check regardles of the submited letter is capital or not it will be found *add functionality new varible that checks how many corect letters the user hasfound *add if statement to display new picture when the letter was not corect and display you gameovermessage when when the last picture has been drawn *add else if state ment to check if all letters has been found and if so display victorymessage *add functionality to only display certain elements when the game has been started HTML: *removed all li items css: * added new propertys and valus att start for letterButtons and guessField ids

--- js/script.js
+++ js/script.js
@@ -9,7 +9,9 @@ var msgElem; // Ger meddelande när spelet är över
 var startGameBtn; // Knappen du startar spelet med
 var letterButtons; // Knapparna för bokstäverna
 var startTime; // Mäter tiden
-var submitGuessBtn;
+var submitGuessBtn; // knapp som submitar användares gissning
+var numberOfCorrectLetters; //visar hur många korekta bokstäver anvädnaren har gissat
+var guessField;
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd
 // Initiering av globala variabler samt koppling av funktioner till knapparna.
 function init() {
@@ -19,12 +21,20 @@ function init() {
     startGameBtn.addEventListener('click',newGame);
     submitGuessBtn = document.querySelector("#userGuessBtn");
     submitGuessBtn.addEventListener('click', letterChecker);
-    
+    hangmanImg = document.querySelector('#hangman');
+    hangmanImgNr = 0; 
+    msgElem = document.querySelector('#message');
+    letterButtons = document.querySelector('#letterButtons');
+    guessField = document.querySelector('#guessField');
+   
+
 } // End init
 
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 function newGame() {
+    letterButtons.style.display = 'inline-flex';
+    guessField.style.display = 'inline';
     wordSelector();
     numberOfLetters();
 
@@ -32,6 +42,8 @@ function newGame() {
 
 function wordSelector() {
     selectedWord = wordList[Math.floor(Math.random()*10)];
+    numberOfCorrectLetters = 0;
+
 }// Funktion som slumpar fram ett ord
  
 function numberOfLetters() {
@@ -52,6 +64,7 @@ function numberOfLetters() {
 function letterChecker() {
     var phraseSplit;
     var selectedWordCopy = "";
+    var uppercaseCopy = "";
     if (/ /.test(selectedWord)) {
         phraseSplit = selectedWord.split(" ");
 
@@ -61,62 +74,55 @@ function letterChecker() {
     } else {
         selectedWordCopy = selectedWord;
     }
+    uppercaseCopy = selectedWordCopy.toUpperCase();
     var userGuess = document.querySelector("#userGuess").value;
-    userGuess.trim();
-    var guessIndex = selectedWordCopy.search(userGuess);
+    userGuess = userGuess.trim();
+    userGuess = userGuess.toUpperCase();
+    var guessIndex = uppercaseCopy.search(userGuess);
     var itsAMatch = false;
     var liList = document.querySelectorAll(".liLetterBoxes");
     if (userGuess.length == 1) {
         for (var k = 0; k < selectedWordCopy.length; k++) {  
-            if(selectedWordCopy.charAt(k) == userGuess) {
-                itsAMatch = true
-                liList[k].innerHTML = userGuess;
+            if(uppercaseCopy.charAt(k) == userGuess) {
+                itsAMatch = true;
+                liList[k].innerHTML = selectedWordCopy.charAt(k);
                 liList[k].classList.add("corect-letter");
+                numberOfCorrectLetters++;
             }
         }
     } else if (userGuess.length > 1 && guessIndex != -1){
         for (var l = 0; l < liList.length; l++ ) {
             if( l == guessIndex) {
-                
-                var userGuessIndex = 0;
+                itsAMatch = true;
                 for (var m = 0; m < userGuess.length; m++) {
-                    liList[l].innerHTML= userGuess.charAt(userGuessIndex);
+                    liList[l].innerHTML= selectedWordCopy.charAt(l);
+                    liList[l].classList.add("corect-letter");
                     l++;
-                    userGuessIndex++;
+                    numberOfCorrectLetters++;
                 }
             }
 
         }
 
     }
-}
-    /*if (guessIndex == -1 && userGuess.length == 1) {
-
-    } else if (guessIndex == -1 && userGuess.length > 1) {
-
-    } else if (userGuess.length == 1) {
-        
-        for (var k = 0; k < liList.length; k++) {
-           if (k == guessIndex) {
-               
-               
-           } 
+    if (!itsAMatch) {
+        hangmanImgNr++;
+        if(hangmanImgNr <= 6) {
+            hangmanImg.src = './images/h' + hangmanImgNr + '.png'; 
+        } else {
+            var gameOverMessege = document.createElement('p');
+            gameOverMessege.innerHTML = "Game Over :'("
+            msgElem.appendChild(gameOverMessege);
         }
-    } else {
-        var liList = document.querySelectorAll(".liLetterBoxes");
-        for (var l = 0 ; l < liList.length; l++ ) {
-            if( l == guessIndex) {
-                var userGuessIndex = 0;
-                for (var m = 0; m < userGuess.length; m++) {
-                    liList[l].innerHTML= userGuess.charAt(userGuessIndex);
-                    l++;
-                    userGuessIndex++;
-                }
-            }
+    } else if (itsAMatch && numberOfCorrectLetters >= selectedWordCopy.length) {
+        var victoryMessage = document.createElement('p');
+            victoryMessage.innerHTML = "You Won!!!!";
+            msgElem.appendChild(victoryMessage);
+            console.log('You did it!');
 
-        }
     }
-}*/// Funktion som körs när du trycker på bokstäverna och gissar bokstav
+
+} /// Funktion som körs när du trycker på bokstäverna och gissar bokstav
 
 // Funktionen ropas vid vinst eller förlust, gör olika saker beroende av det
 
